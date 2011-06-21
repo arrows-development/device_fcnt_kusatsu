daff73bb41443cfc69fca9f86e23e0ec0bd287e4
Benoit Goby <benoit@android.com>
2011-06-21 16:59:27 -0700

    manaus: Copy android.hardware.telephony.gsm.xml
    Change-Id: Ibef525ab5e37fd35867b4251ad48a58db9fe68f3

--- device.mk
+++ device.mk
@@ -197,6 +197,7 @@ PRODUCT_PACKAGES += \
     vendor_mdota_symlink
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
