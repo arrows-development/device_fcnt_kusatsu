7bf18024b2dbbaa2d9b213379ed00a43e98d01e0
Wang Han <416810799@qq.com>
2019-05-13 20:33:41 +0800

    manaus: Set AAPT configuration
    Change-Id: I0aa83727943ac59069f1d27496a2a1b5855cf607

--- device.mk
+++ device.mk
@@ -9,6 +9,10 @@ DEVICE_PATH := device/motorola/manaus
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
