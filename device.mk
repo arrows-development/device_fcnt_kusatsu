d4e6cb5455548eac97796be3bb0e48214fc6ca07
Benedict Wong <benedictwong@google.com>
2019-07-16 12:57:20 -0700

    manaus: Add feature flag for FEATURE_IPSEC_TUNNELS
    This commit adds the IPSEC_TUNNELS feature flag, with XFRM Interfaces
    used as the implementation of choice.
    
    Bug: 117183273
    Bug: 137629222
    Test: Compiles, passes tunnel-mode CTS tests
    Test: adb shell pm list features
    Change-Id: I19c598947525783850b29d149db85db89f8b347e

--- device.mk
+++ device.mk
@@ -196,6 +196,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor_mdota_symlink
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
