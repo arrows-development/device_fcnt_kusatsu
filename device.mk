af242ec2d025bb919e244edbeafb3381e8368079
Jake Weinstein <jake@aospa.co>
2023-04-15 02:36:31 +0900

    manaus: Enable alternative brightness algorithm for low gamma
    Change-Id: I6f141ecfb3b71cd3509f66da299251ce7d1f7614

--- device.mk
+++ device.mk
@@ -113,6 +113,9 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Display
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    sys.brightness.disable_gamma_conversion=true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
