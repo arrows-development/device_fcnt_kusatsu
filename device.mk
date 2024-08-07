a892a2ea92a73f60e81a3d0e65db62db5f7d94b9
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 01:49:56 +0300

    manaus: fingerprint: rtwo -> manaus
    Change-Id: I6d2aee48b48572ed8ff116411646193f5ed2b701

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot-service.example_recovery \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.manaus
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
