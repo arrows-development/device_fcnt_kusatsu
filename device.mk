2088c3ccbf7006938a5dca8c4d72540b2adf6360
Yan Yan <evitayan@google.com>
2025-03-04 23:52:31 +0100

    manaus: Add feature flag for IPSEC_TUNNEL_MIGRATION
    This commit adds the IPSEC_TUNNEL_MIGRATION feature flag,
    with XFRM_MIGRATE used as the implementation
    
    Bug: 273379472
    Test: build, flash and verify with "adb shell pm list features"
    Test: atest IpSecManagerTunnelTest
    Change-Id: Id2ee3e65183e32ea63f6251ba921e1e4c6b09ed0

--- device.mk
+++ device.mk
@@ -197,7 +197,8 @@ PRODUCT_PACKAGES += \
     vendor_mdota_symlink
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
 # Sensors
 PRODUCT_PACKAGES += \
