309afd895d267e145cde9df56017d59b075277d2
sabrina010209 <sebidk700@gmail.com>
2025-07-01 19:39:35 -0500

    manaus: Define A/B partitions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,19 @@
 
 DEVICE_PATH := device/motorola/manaus
 
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+    boot \
+    system \
+    system_ext \
+    product \
+    vendor \
+    vendor_dlkm \
+    vendor_boot \
+    vbmeta \
+    vbmeta_system
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
