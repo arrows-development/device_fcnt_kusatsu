81896b74ce8f85d33282df42a130771e9a20400a
Simao Gomes Viana <xdevs23@outlook.com>
2025-07-05 08:08:21 -0500

    manaus: Configure dalvik-heap for 8GiB
    * Values grabbed from https://github.com/PixelDust-Eleven/android_vendor_pixeldust/blob/3894c9ab9a0b730d326b5a619628d48708f4a21d/configs/phone-xhdpi-8192-dalvik-heap.mk#L19-L25
    Co-authored-by: Simao Gomes Viana <xdevs23@outlook.com>
    Change-Id: I32f383be1ee16f14dc9a5ae789884a10f717ce14
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -112,6 +112,15 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=24m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.46 \
+    dalvik.vm.heapminfree=8m \
+    dalvik.vm.heapmaxfree=48m
+
 # Dex-pre-opt exclusions
 $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
 
