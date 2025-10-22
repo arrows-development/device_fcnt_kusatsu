9fb6425eea15aec42b840925c84d637f3332389b
Giovanni Ricca <giovanniricca@proton.me>
2025-10-22 15:09:21 +0200

    manaus: fingerprint: Set `LOG_TAG` via cflag
    Change-Id: Iee847f89a38a8b0fc460e5ab1caf9e7f8936e8aa

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.manaus"
-
 #include "BiometricsFingerprint.h"
 
 #include <android-base/file.h>
