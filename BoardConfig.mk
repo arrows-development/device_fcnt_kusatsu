arrows-development/device_fcnt_fuji

manaus: Define arch

BoardConfig.mk
@@ -6,5 +6,18 @@
 
 DEVICE_PATH := device/motorola/manaus
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a78
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+
 # Inherit the proprietary files
 include vendor/motorola/manaus/BoardConfigVendor.mk

arrows-development/device_fcnt_kusatsu

BoardConfig.mk
@@ -6,5 +6,18 @@
 
 DEVICE_PATH := device/motorola/manaus
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+
 # Inherit the proprietary files
 include vendor/motorola/manaus/BoardConfigVendor.mk