bcf39074eb46d382b7b07329a207d69573aeeaec
Mashopy <eliasgheeraert@gmail.com>
2026-05-26 19:51:47 -0500

    manaus: Drop in-tree mdota symlink
    * Now we switch to the one from hardware/mediatek
    
    Change-Id: I962c2b9169694112ea1a8b7db7a70da8f8afe741

--- device.mk
+++ device.mk
@@ -276,7 +276,7 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PACKAGES += \
-    vendor_mdota_symlink
+    mdota_symlink
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
