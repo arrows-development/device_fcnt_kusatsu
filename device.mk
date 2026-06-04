e0a5396bc0c9a6af1a9b6b193fb9c66f5d5b620e
rexix01 <sebidk700@gmail.com>
2026-06-03 22:13:48 -0500

    manaus: Configure egis fingerprint
    co-authored-by: electimon <electimon@gmail.com>
    co-authored-by: TheScarastic <warabhishek@gmail.com>
    Change-Id: I235fff9db919a02f2ac3255d7fe0c64d9740fad0

--- device.mk
+++ device.mk
@@ -168,7 +168,11 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.manaus
+    android.hardware.biometrics.fingerprint@2.3-service.manaus \
+    android.hardware.biometrics.fingerprint@2.3-service.manaus2
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
