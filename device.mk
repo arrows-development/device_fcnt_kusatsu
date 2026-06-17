ee6b02dce7d0727e6e1e2bb6f9f1fb4c6eb9d236
Mashopy <eliasgheeraert@gmail.com>
2026-06-17 18:26:56 +0000

    manaus: Use common mediatek logtag properties

--- device.mk
+++ device.mk
@@ -289,6 +289,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Properties
+include hardware/mediatek/configs/properties/vendor_logtag.mk
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
